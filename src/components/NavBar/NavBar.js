08c79fc6767cd36afe7716746ccd1010554fe731
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-06-21 22:04:21 +0200

    save

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import { Form, FormControl, Button, Nav, Container, Navbar } from 'react-bootstrap';
+
+export default function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg" className=" navbar-dark bg-dark">
+      <Container fluid>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="btn-sm" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Button variant="primary">Primary</Button>{' '}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
