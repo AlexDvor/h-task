b99d1b24062e6a8b9dbfb9e15ef010fb35d0ca2d
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-06-24 16:53:59 +0200

    add  new login form

--- src/components/LoginForm/LoginFrom.js
+++ src/components/LoginForm/LoginFrom.js
@@ -1,84 +1,123 @@
-// import { useState } from 'react';
+import { useState } from 'react';
+import { Form, Button, Spinner, Card, InputGroup } from 'react-bootstrap';
 import s from './LoginForm.module.css';
 
 export default function LoginForm() {
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [validated, setValidated] = useState(false);
-  // const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [validated, setValidated] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
-  // const handleSubmit = event => {
-  //   const form = event.currentTarget;
-  //   if (form.checkValidity() === false) {
-  //     event.preventDefault();
-  //     event.stopPropagation();
-  //     setValidated(true);
-  //     return;
-  //   }
-  //   event.preventDefault();
-  //   setLoading(true);
-  //   console.log({ email, password });
-  // };
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      setValidated(true);
+      return;
+    }
+
+    setLoading(true);
+    console.log({ email, password });
+  };
 
   return (
     <>
-      <div className="d-flex justify-content-center ">
-        <div className={s.card}>
-          <div className={s.cardHeader}>
-            <h3>Sign In</h3>
-            <div className={`d-flex justify-content-end ${s.socialIcon}`}>
-              <span>
-                <i className="fab fa-facebook-square"></i>
-              </span>
-              <span>
-                <i className="fab fa-google-plus-square"></i>
-              </span>
-              <span>
-                <i className="fab fa-twitter-square"></i>
-              </span>
-            </div>
-          </div>
+      <Card className={`${s.wrapperCard}`}>
+        <Card.Header className={`${s.cardHeader}`}>
+          <Card.Title className={`${s.cardTitle}`}>Sign In</Card.Title>
+          <div className={`${s.socialIcon}`}>
+            <span>
+              <i className="fab fa-facebook-square"></i>
+            </span>
 
-          <div className="card-body">
-            <form>
-              <div className="input-group form-group mb-3">
-                <div className={s.inputGroupPrepend}>
-                  <span className="input-group-text">
-                    <i className="fas fa-user"></i>
-                  </span>
-                </div>
-                <input type="text" className="form-control" placeholder="username" />
-              </div>
-              <div className="input-group form-group mb-3">
-                <div className={s.inputGroupPrepend}>
-                  <span className="input-group-text">
-                    <i className="fas fa-key"></i>
-                  </span>
-                </div>
-                <input type="password" className="form-control" placeholder="password" />
-              </div>
-              <div className={`row align-items-center ${s.remember}`}>
-                <input type="checkbox" />
-                Remember Me
-              </div>
-              <div className="form-group d-flex justify-content-end">
-                <button type="submit" value="Login" className={`btn float-right ${s.loginBtn}`}>
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
+            <span>
+              <i className="fab fa-google-plus-square"></i>
+            </span>
 
-          <div className="card-footer">
-            <div className={`d-flex justify-content-center ${s.links}`}>
-              Don't have an account?<a href="#">Sign Up</a>
-            </div>
-            <div className="d-flex justify-content-center">
-              <a href="#">Forgot your password?</a>
-            </div>
+            <span>
+              <i className="fab fa-twitter-square"></i>
+            </span>
           </div>
-        </div>
-      </div>
+        </Card.Header>
+
+        <Card.Body>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <InputGroup className={`${s.inputGroup}`}>
+              <InputGroup.Text>
+                <i className="fas fa-user"></i>
+              </InputGroup.Text>
+              <Form.Control
+                required
+                type="email"
+                placeholder="email"
+                autoComplete="true "
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                className={`${s.inputField}`}
+              />
+              <Form.Control.Feedback type="invalid">
+                Introduzca una dirección de correo electrónico válida!
+              </Form.Control.Feedback>
+              <Form.Control.Feedback type="valid">Perfecto!</Form.Control.Feedback>
+            </InputGroup>
+
+            <InputGroup className={`${s.inputGroup}`}>
+              <InputGroup.Text>
+                <i className="fas fa-key"></i>
+              </InputGroup.Text>
+              <Form.Control
+                required
+                type="password"
+                placeholder="password"
+                autoComplete="true "
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                className={`${s.inputField}`}
+              />
+              <Form.Control.Feedback type="invalid">
+                Este campo es obligatorio
+              </Form.Control.Feedback>
+              <Form.Control.Feedback type="valid">Perfecto!</Form.Control.Feedback>
+            </InputGroup>
+
+            <div className={`${s.buttonsWrapper}`}>
+              {isLoading ? (
+                <>
+                  <Button className={`${s.loginBtn} ${s.isLoadingBtn}`} type="submit" disabled>
+                    <Spinner
+                      as="span"
+                      animation="grow"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                    Loading...
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button className={`${s.loginBtn}`} type="submit">
+                    Login
+                  </Button>
+                </>
+              )}
+            </div>
+          </Form>
+        </Card.Body>
+
+        <Card.Footer>
+          <div className={`${s.links}`}>
+            Don't have an account?
+            <Card.Link className={`${s.signUpLink}`} href="#">
+              Sign Up
+            </Card.Link>
+            <Card.Link className={`${s.passwordLink}`} href="#">
+              Forgot your password?
+            </Card.Link>
+          </div>
+        </Card.Footer>
+      </Card>
     </>
   );
 }
