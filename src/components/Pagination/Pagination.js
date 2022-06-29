31e265229dc93db0e05fd2ab162720bc8a8556f4
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-06-29 20:27:43 +0200

    ref pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,7 +1,10 @@
 import s from './Pagination.module.css';
+import { useState } from 'react';
 
 export default function PaginationUsers(props) {
   const { postsPerPage, totalPosts, paginate, nextPage, prevPage, currPage } = props;
+  const [isDisabledNextBtn, setIsDisabledNextBtn] = useState(false);
+  const [isDisabledPreviousBtn, setIsDisabledPreviousBtn] = useState(false);
 
   const pageNumbers = [];
   const quantityPages = Math.ceil(totalPosts / postsPerPage);
@@ -9,6 +12,33 @@ export default function PaginationUsers(props) {
   for (let i = 1; i <= quantityPages; i++) {
     pageNumbers.push(i);
   }
+
+  const checkingForLastElement = currentPage => {
+    if (currentPage !== quantityPages) {
+      nextPage();
+      setIsDisabledNextBtn(false);
+      setIsDisabledPreviousBtn(false);
+    } else {
+      setIsDisabledNextBtn(true);
+      return;
+    }
+  };
+
+  const onPageItemClick = currentPage => {
+    paginate(currentPage);
+    setIsDisabledNextBtn(false);
+    setIsDisabledPreviousBtn(false);
+  };
+
+  const checkingForFirstElement = currentPage => {
+    if (currentPage !== 1) {
+      prevPage();
+      setIsDisabledPreviousBtn(false);
+      setIsDisabledNextBtn(false);
+    } else {
+      setIsDisabledPreviousBtn(true);
+    }
+  };
 
   return (
     <>
@@ -18,21 +48,50 @@ export default function PaginationUsers(props) {
         </div>
         <ul className="pagination justify-content-center">
           <li className="page-item">
-            <button className={s.pageButtonLink} onClick={() => prevPage()}>
-              Previous
-            </button>
+            {!isDisabledPreviousBtn ? (
+              <>
+                <button
+                  className={s.pageButtonLink}
+                  onClick={() => checkingForFirstElement(currPage)}
+                >
+                  Previous
+                </button>
+              </>
+            ) : (
+              <>
+                <button className={s.pageButtonLink} disabled>
+                  Previous
+                </button>
+              </>
+            )}
           </li>
           {pageNumbers.map(num => (
             <li className="page-item" key={num}>
-              <button className={s.pageButtonLink} onClick={() => paginate(num)}>
+              <button
+                className={`${s.pageButtonLink} ${currPage === num ? s.selectedPage : ''}`}
+                onClick={e => onPageItemClick(num)}
+              >
                 {num}
               </button>
             </li>
           ))}
           <li className="page-item">
-            <button className={s.pageButtonLink} onClick={() => nextPage()}>
-              Next
-            </button>
+            {!isDisabledNextBtn ? (
+              <>
+                <button
+                  className={s.pageButtonLink}
+                  onClick={e => checkingForLastElement(currPage)}
+                >
+                  Next
+                </button>
+              </>
+            ) : (
+              <>
+                <button className={s.pageButtonLink} disabled>
+                  Next
+                </button>
+              </>
+            )}
           </li>
         </ul>
       </div>
